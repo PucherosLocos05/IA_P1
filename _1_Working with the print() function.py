1b8be6fc129c9f33a9a0c02fe2daf078bc8c2ab7
SoftwareHero05 <A23310372@live.ceti.mx>
2026-02-21 20:07:42 -0600

    Primeras tres practicas del parcial
    se agregan las primeras tres practcias del parcial

---
+++ _1_Working with the print() function.py
@@ -0,0 +1,10 @@
+#Jesus Antonio Baez Ortega 23310372 6E
+#LAB#1   Working with the print() function
+
+print ("Jesus")
+print ('Jesus')
+print (Jesus)
+print ("Jesus") print (Python)
+print "Jesus"
+
+
